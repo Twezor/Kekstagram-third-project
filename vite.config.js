d0c3f81528f6fbf775a1dbd9b14d3fd3d84c2914
Grigory Musikhin <grintwezor93@gmail.com>
2026-01-31 20:28:55 +0300

    Add vite-plugin-static-copy for assets

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,21 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
-import { cpSync } from 'fs';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
   base: './',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      input: {
-        // eslint-disable-next-line no-undef
-        main: resolve(__dirname, 'index.html')
-      }
-    }
+    assetsDir: 'assets'
   },
   plugins: [
-    {
-      name: 'copy-static-assets',
-      closeBundle() {
-        // Копируем vendor
-        cpSync('vendor/nouislider', 'dist/vendor/nouislider', { recursive: true });
-        cpSync('vendor/pristine', 'dist/vendor/pristine', { recursive: true });
-        // Копируем картинки
-        cpSync('img', 'dist/img', { recursive: true });
-        cpSync('photos', 'dist/photos', { recursive: true });
-        // Копируем fonts если есть
-        // eslint-disable-next-line no-undef
-        if (fs.existsSync('fonts')) {
-          cpSync('fonts', 'dist/fonts', { recursive: true });
-        }
-      }
-    }
+    viteStaticCopy({
+      targets: [
+        { src: 'vendor/nouislider/**/*', dest: 'vendor/nouislider' },
+        { src: 'vendor/pristine/**/*', dest: 'vendor/pristine' },
+        { src: 'img/**/*', dest: 'img' },
+        { src: 'photos/**/*', dest: 'photos' },
+        { src: 'fonts/**/*', dest: 'fonts' }
+      ]
+    })
   ]
 });
